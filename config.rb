334eb85fa12d1dcd9db19a92750096002c0d33ef
sailxjx <sailxjx@163.com>
2012-09-20 10:52:32 +0800

    change theme to slash

--- config.rb
+++ config.rb
@@ -4,13 +4,13 @@ project_type = :stand_alone
 # Publishing paths
 http_path = "/blog"
 http_images_path = "/blog/images"
-http_fonts_path = "/blog/fonts"
+http_fonts_path = "/blog/font"
 css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
 images_dir = "source/images"
-fonts_dir = "source/fonts"
+fonts_dir = "source/font"
 
 line_comments = false
 output_style = :compressed
